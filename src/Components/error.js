0ce72548e6d21002447ea5f9ee5972f701f47a6d
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-09-05 19:05:56 +0300

    editing the form

--- src/Components/error.js
+++ src/Components/error.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import Figure from 'react-bootstrap/Figure';
+import Alert from 'react-bootstrap/Alert'
 
 class Error extends Component{
     
@@ -14,7 +15,13 @@ class Error extends Component{
     src="https://31.media.tumblr.com/2e8986a1b1c062623cea1b9edaddcc52/tumblr_mup3qzOPsX1rk0k2jo1_500.gif"
   />
   <Figure.Caption>
-  Something went wrong {this.props.errormsg}
+  <Alert variant="danger">
+  Something went wrong with{' '}
+  <Alert.Link href="https://blog.thomasnet.com/industrial-website-404-error"> Not Found {this.props.errormsg}</Alert.Link>. Give it a click if you
+    you wanna find out more.
+  </Alert>
+  
+  
   </Figure.Caption>
 </Figure>
             </>
