4540a4ad4ce4b13e48289003e8e9f28e338ba2b2
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-09-07 02:14:19 +0300

    rendering the content

--- src/App.js
+++ src/App.js
@@ -1,112 +1,133 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Form from './Components/form';
 import Header from './Components/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import  '../src/App.css';
+import '../src/App.css';
 import axios from "axios";
 import Body from "./Components/body";
 import Error from "./Components/error";
 import { AxiosResponse, AxiosError } from 'axios';
 
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props)
-    this.state={
-      lat:'',
-      lon:'',
-      cityName:'',
+    this.state = {
+      lat: '',
+      lon: '',
+      cityName: '',
       showBody: false,
       imageSrc: '',
       displayErr: false,
-      errormsg:'',
-      description:'',
-      date:'',
-      wind:'',
-      
+      errormsg: '',
+      firstDayDate: '',
+      firstDayWeather: '',
+      firstDayWind: '',
+      secondDayDate: '',
+      secondDayWeather: '',
+      secondDayWind: '',
+      thirdDayDate: '',
+      thirdDayWeather: '',
+      thirdDayWind: '',
     }
   }
-  
-  getMapLocation= async (event) => {
-  
- 
+
+  getMapLocation = async (event) => {
+
+
     event.preventDefault()
-  
-  const cityName= event.target.cityName.value ;
-  const Key= `pk.2cae4dc102199ef3d69e64cbe4bca40a`;
-  const url= `https://us1.locationiq.com/v1/search.php?key=${Key}&q=${cityName}&format=json`;
 
-  try{
-    const selectedCity = await axios.get(url).catch((error) => {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      }
+    const cityName = event.target.cityName.value;
+    const Key = `pk.2cae4dc102199ef3d69e64cbe4bca40a`;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${Key}&q=${cityName}&format=json`;
+
+    try {
+      const selectedCity = await axios.get(url).catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+        this.setState({
+          displayErr: true,
+          showBody: false,
+          errormsg: error.response.status
+        })
+
+      });
       this.setState({
-        displayErr:true,
-        showBody:false,
-        errormsg: error.response.status
-      })
-   
-    });
-    this.setState({
-      lat: selectedCity.data[0].lat,
-      lon: selectedCity.data[0].lon,
-      cityName: selectedCity.data[0].display_name,
-      showBody:true,
-      
-      
-    })
- 
-    const herokuUrl = `https://city-explorer-api-bk201.herokuapp.com/weather?cityName=${cityName}`
-    const  cityInfo = await axios.get(herokuUrl).catch((error)=>{
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      }
-   
-    })
-    this.setState({
-      description: cityInfo.data[0],
-      date: cityInfo.data[1],
-      wind:cityInfo.data[2],
-      
-    })
-    console.log(cityInfo.data[0],cityInfo.data[1],cityInfo.data[2]);
-  }
+        lat: selectedCity.data[0].lat,
+        lon: selectedCity.data[0].lon,
+        cityName: selectedCity.data[0].display_name,
+        showBody: true,
 
-  catch{
-    
- 
-  }
-  
-  }
-  render(){
-    return(
-    <>
-  
-    <Header></Header>
-    <Form getMapLocation = {this.getMapLocation} ></Form>
-    {this.state.showBody &&
-      <Body 
-      lat={this.state.lat} 
-      lon= {this.state.lon}
-      cityName= {this.state.cityName}
-      description={this.state.description}
-      date={this.state.date}
-      wind={this.state.wind}
-      
-      >
-      
-      </Body>
-     
+
+      })
+      const herokuUrl = `https://city-explorer-api-bk201.herokuapp.com/weather?cityName=${cityName}`
+      const cityInfo = await axios.get(herokuUrl).catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      })
+
+      // one hunder precent sure that we should loop instead of doing it like this 
+      this.setState({
+        firstDayDate: cityInfo.data[0].date,
+        firstDayWeather: cityInfo.data[0].description,
+        firstDayWind: cityInfo.data[0].wind,
+        secondDayDate: cityInfo.data[1].date,
+        secondDayWeather: cityInfo.data[1].description,
+        secondDayWind: cityInfo.data[1].wind,
+        thirdDayDate: cityInfo.data[2].date,
+        thirdDayWeather: cityInfo.data[2].description,
+        thirdDayWind: cityInfo.data[2].wind,
+
+      })
+
+
     }
-    {this.state.displayErr&& 
-    <Error errormsg={this.state.errormsg}></Error>}
-  
-    </>
+
+
+    catch {
+
+
+    }
+
+    console.log(this.state.firstDayDate, this.state.firstDayWeather, this.state.firstDayWind);
+  }
+  render() {
+    return (
+      <>
+
+        <Header></Header>
+        <Form getMapLocation={this.getMapLocation} ></Form>
+        {this.state.showBody &&
+          <Body
+            lat={this.state.lat}
+            lon={this.state.lon}
+            cityName={this.state.cityName}
+            description={this.state.description}
+            firstDayDate={this.state.firstDayDate}
+            firstDayWeather={this.state.firstDayWeather}
+            firstDayWind={this.state.firstDayWind}
+            secondDayDate={this.state.secondDayDate}
+            secondDayWeather={this.state.secondDayWeather}
+            secondDayWind={this.state.secondDayWind}
+            thirdDayDate={this.state.thirdDayDate}
+            thirdDayWeather={this.state.thirdDayWeather}
+            thirdDayWind={this.state.thirdDayWind}
+
+          >
+
+          </Body>
+
+        }
+        {this.state.displayErr &&
+          <Error errormsg={this.state.errormsg}></Error>}
+
+      </>
     )
   }
 }
--- src/Components/body.js
+++ src/Components/body.js
@@ -9,23 +9,32 @@ class bodyClass extends Component {
     render() {
         return (
             <>
-            <br></br>
-            <Card style={{ width: '30rem' }} className="bg-dark text-white">
-  <Card.Img  src={`https://maps.locationiq.com/v3/staticmap?key=pk.2cae4dc102199ef3d69e64cbe4bca40a&center=${this.props.lat},${this.props.lon}`} alt="Card image" />
-  <Card.ImgOverlay>
-    <Card.Title style={{ color: 'black' }}>{this.props.cityName}</Card.Title>
-    <Card.Title style={{ color: 'black' }}> Weather :{this.props.description}</Card.Title>
-    <Card.Title style={{ color: 'black' }}>{this.props.date}</Card.Title>
-    <Card.Title style={{ color: 'black' }}>Wind Direction : {this.props.wind}</Card.Title>
-    <Card.Title style={{ color: 'black' }}>Lat : {this.props.lat}</Card.Title>
-    <Card.Title style={{ color: 'black' }}>Lon : {this.props.lon}</Card.Title>
-    <Card.Text style={{ color: 'black' }}>
-        {` This is ${this.props.cityName} `}
-    </Card.Text>
- 
-  </Card.ImgOverlay>
-</Card>
-             
+                <br></br>
+                <Card style={{ width: '30rem' }} className="bg-dark text-white">
+                    <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=pk.2cae4dc102199ef3d69e64cbe4bca40a&center=${this.props.lat},${this.props.lon}`} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title style={{ color: 'black' }}>{this.props.cityName}</Card.Title>
+                        <Card.Title style={{ color: 'black' }}>Lat : {this.props.lat}</Card.Title>
+                        <Card.Title style={{ color: 'black' }}>Lon : {this.props.lon}</Card.Title>
+                        <Card.Text style={{ color: 'black' }}>
+                            {` This is ${this.props.cityName} `}
+
+                        </Card.Text>
+
+                    </Card.ImgOverlay>
+                </Card>
+                <Card  style={{ width: '30rem' }} className="bg-dark text-white">
+                <h2>Day 1 : {this.props.firstDayDate}</h2>
+                <h2>Day 1 weather : {this.props.firstDayWeather}</h2>
+                <h2>Day 1 wind : {this.props.firstDayWind}</h2>
+                <h2>Day 2 : {this.props.secondDayDate}</h2>
+                <h2>Day 2 weather : {this.props.secondDayWeather}</h2>
+                <h2>Day 2 wind : {this.props.secondDayWind}</h2>
+                <h2>Day 3  : {this.props.thirdDayDate}</h2>
+                <h2>Day 3 weather : {this.props.thirdDayWeather}</h2>
+                <h2>Day 3 wind : {this.props.thirdDayWind}</h2>
+                </Card>
+
             </>
         )
     }
